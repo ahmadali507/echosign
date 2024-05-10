eba7afc5cb902e2833a9004fd319c8b1e05f3c00
naumanch969 <naumanch969@gmail.com>
2024-05-10 21:52:13 +0500

    redux in progress

---
+++ client/src/interfaces/index.ts
@@ -0,0 +1,14 @@
+
+export interface User {
+  id?: string;
+  name: string;
+  email: string;
+  phone: string;
+  bio: string;
+  country: string;
+  state: string;
+  city: string;
+  zipCode: string;
+  partnerId: string;
+  photoUrl: string; 
+} 
---
+++ client/src/store/reducers/authSlice.ts
@@ -0,0 +1,56 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+
+
+interface AuthState {
+  auth: User | null;
+  isLoading: boolean;
+  error: { message: string; code: string } | null;
+}
+
+
+export const signUp = createAsyncThunk<>('auth/signUp', async (userCredentials) => {
+  try {
+
+
+  } catch (error: Error) {
+    throw error.message;
+  }
+});
+
+const initialState: AuthState = {
+  auth: null,
+  isLoading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    resetState: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signUp.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.auth = action.payload;
+        localStorage.setItem('auth', JSON.stringify(action.payload));
+      })
+      .addCase(signUp.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = { message: action.error.message || '', code: action.error.code || '' };
+      })
+      .addCase(authActions.resetState, () => {
+        return initialState;
+      });
+  },
+});
+
+export default authSlice.reducer;
+export const selectAuth = (state: RootState) => state.auth.auth;
+export const { actions: authActions } = authSlice;
