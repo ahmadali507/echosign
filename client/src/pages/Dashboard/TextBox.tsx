6356c6411ca625f0e496b8d290194c4507eb4ada
Yusra <yusrazainab.xb.senior@gmail.com>
2024-05-12 11:20:14 +0500

    Voice options added

--- client/src/pages/Dashboard/TextBox.tsx
+++ client/src/pages/Dashboard/TextBox.tsx
@@ -1,48 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/*
-import { Button } from '@/components/ui/button'
-import { useState } from 'react';
-
-const TextBox = () => {
-    const [text, setText] = useState('');
-    const [speaking, setSpeaking] = useState(false);
-    const synth = window.speechSynthesis;
-  
-    const speakText = () => {
-      if (synth.speaking) {
-        return;
-      }
-      const utterance = new SpeechSynthesisUtterance(text);
-      setSpeaking(true);
-      utterance.onend = () => {
-        setSpeaking(false);
-      };
-      synth.speak(utterance);
-    };
-    
-    /////////////////////////////////////////////////////// RENDER //////////////////////////////////////////////////////////
-    return (
-        <div className="w-full h-full rounded-lg flex flex-col gap-1 ">
-            <h2 className='text-xl font-medium ' >Text</h2>
-
-        <textarea className="w-full h-full bg-gray-100 rounded-lg p-4 text-black text-lg resize-none "
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Type text here..."
-          rows={6}
-          cols={50}
-        />
-        <Button onClick={speakText} disabled={!text || speaking}>
-          {speaking ? 'Converting' : 'Convert'}
-        </Button>
-      </div>
-    )
-}
-
-export default TextBox
-*/
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from '@/components/ui/button';
 import { useState, useEffect } from 'react';
 
