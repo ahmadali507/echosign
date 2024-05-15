60740409306936473430276f52e8c2523d30ab5e
naumanch969 <naumanch969@gmail.com>
2024-05-15 16:38:49 +0500

    Adding api

--- client/src/pages/Dashboard/Mimick.tsx
+++ client/src/pages/Dashboard/Mimick.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button"
 import { useStateContext } from "@/context/useStateContext"
 
 const Mimick = () => {
 
     const { capturedImage } = useStateContext()
+
+    const onExport = ()=>{
+        
+    }
 
     return (
         <div className='w-full h-full rounded-lg flex flex-col gap-1 ' >
@@ -12,14 +17,16 @@ const Mimick = () => {
             <div className="w-full h-full bg-gray-100 rounded-lg overflow-hidden ">
                 {
                     capturedImage
-                    ?
-                    <div className="text-center flex justify-center items-center w-full h-full ">
-                        Captured image will be shown here
-                    </div>
-                    :
-                    <img src={capturedImage} alt='' className='w-full h-full object-cover ' />
+                        ?
+                        <img src={capturedImage} alt='' className='w-full h-full object-cover ' />
+                        :
+                        <div className="text-center flex justify-center items-center w-full h-full ">
+                            Captured image will be shown here
+                        </div>
                 }
             </div>
+
+            <Button onClick={onExport} >Send</Button>
 
         </div>
     )
